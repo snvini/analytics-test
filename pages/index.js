7807640a369f2b8f06a267c8dce150122ebb589b
Vinicius Alvim <vinicius.alvim@sankhya.com.br>
2021-01-06 15:58:47 -0300

    teste rapido

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,59 @@ ReactGA.pageview('Home');
 function Contador(){
     const [contador,setContador] = useState(0);
 
-    function adiciona(){
+    function add1(){
         setContador(contador +1);
         ReactGA.event({
-            category: 'User',
-            action: 'Created an Account'
-        });
-
-        ReactGA.event({
-            category: 'Social',
-            action: 'Rated an App',
+            category: 'navegacao',
+            label: 'App1',
             value: 3
+        });
+    }
+
+    function add2(){
+        setContador(contador +1);
+        ReactGA.event({
+            category: 'navegacao',
+            label: 'App3',
+            value: 2
+        });
+    }
+
+    function add3(){
+        setContador(contador +1);
+        ReactGA.event({
+            category: 'navegacao',
+            label: 'App2',
+            value: 2
+        });
+    }
+
+    function add4(){
+        setContador(contador +1);
+        ReactGA.event({
+            category: 'navegacao',
+            label: 'App1',
+            value: 2
+        });
+    }
+
+    function add5(){
+        setContador(contador +1);
+        ReactGA.event({
+            category: 'navegacao',
+            label: 'App3',
+            value: 1
         });
     }
 
     return (
         <div>
             <div>{contador}</div>
-            <button onClick={adiciona}>Contar</button>
+            <button onClick={add1}>[1]</button>
+            <button onClick={add2}>[2]</button>
+            <button onClick={add3}>[3]</button>
+            <button onClick={add4}>[4]</button>
+            <button onClick={add5}>[5]</button>
         </div>
     )
 }
